ee5aa5e2c0a93473cda59ccd20eea3c78d1aeb45
PrathameshSonje <prathameshsonje147@gmail.com>
2023-12-31 02:03:58 +0530

    added context files and basic frontend

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,12 +1,19 @@
 import http from 'http';
+import SocketService from './services/socket';
 
 const init = async () => {
+    const socketServive = new SocketService();
+
     const httpServer = http.createServer();
     const PORT = process.env.PORT ? process.env.PORT : 8000;
 
+    socketServive.io.attach(httpServer);
+
     httpServer.listen(PORT, () => {
         console.log(`server is listening on ${PORT}`);
-    })
+    });
+
+    socketServive.initListener();
 }
 
 init();
--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -1,1 +1,33 @@
-class 
+import { Server } from "socket.io";
+
+class SocketService {
+    private _io: Server;
+
+    constructor() {
+        console.log('Init Socker Service');
+        this._io = new Server({
+            cors: {
+                allowedHeaders: ["*"],
+                origin: "*",
+            }
+        });
+    }
+
+    public initListener() {
+        const io = this._io;
+        console.log('Init Socket listeners');
+        
+        io.on('connect', (socket) => {
+            console.log(`New socket connected`, socket.id);
+            socket.on('event:message', async ({message}: {message: string}) => {
+                console.log('new message rec..', message);
+            });
+        });
+    }
+
+    get io() {
+        return this._io
+    }
+}
+
+export default SocketService;
